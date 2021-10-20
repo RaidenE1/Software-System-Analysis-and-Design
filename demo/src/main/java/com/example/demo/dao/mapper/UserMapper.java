c2790aafa2971a03a788dfac5794f3821abc9f5b
syx1923 <19231179@buaa.edu.cn>
2021-10-20 16:26:58 +0800

    添加修改用户名和密码的xml文件内容

--- demo/src/main/java/com/example/demo/dao/mapper/UserMapper.java
+++ demo/src/main/java/com/example/demo/dao/mapper/UserMapper.java
@@ -9,8 +9,6 @@ public interface UserMapper {
 
 	int createUser(User user);
 
-	User getByEmail(String email);
-
 	void changeUsername(int id, String newUsername);
 
 	void changeUserPassword(int id, String newPassword);
