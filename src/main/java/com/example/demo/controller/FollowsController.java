7e55de4721e0eba0d4229fac2067ee66e914e267
rzhevcherkasy <71552000+rzhevcherkasy@users.noreply.github.com>
2021-11-22 23:05:04 +0800

    部分完成的学术分享后端
    部分完成的学术分享后端

--- src/main/java/com/example/demo/controller/FollowsController.java
+++ src/main/java/com/example/demo/controller/FollowsController.java
@@ -24,7 +24,7 @@ public class FollowsController {
     private UserService userService;
 
     @PostMapping("/getFollowers")
-    public Map<String, Object> getFollowers(@RequestBody Map<String, String> remap){
+    public Object getFollowers(@RequestBody Map<String, String> remap){
         Map<String, Object> resMap = new HashMap<>();
         int userid = Integer.parseInt(remap.get("userid"));
         List<Follows> follows_list = followsService.getFollowsByUserid(userid);
@@ -40,7 +40,7 @@ public class FollowsController {
     public Object getFollowedUser(@RequestBody Map<String, String> remap){
         Map<String, Object> resMap = new HashMap<>();
         int userid = Integer.parseInt(remap.get("userid"));
-        List<Follows> follow_list = followsService.getFollowsByFollowerUserid(userid)
+        List<Follows> follow_list = followsService.getFollowsByFollowerUserid(userid);
         List<User> follow_users_list = new ArrayList<>();
         for (Follows f : follow_list) {
             User follower = userService.getUserById(f.getFollower_userid());
