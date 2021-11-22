e372b8e36002fd4c675e51a29288ad004381a2b1
jinhezhang17 <jinhezhang17@outlook.com>
2021-11-22 21:47:24 +0800

    add user's attributes

--- src/main/java/com/example/demo/domain/entity/User.java
+++ src/main/java/com/example/demo/domain/entity/User.java
@@ -20,4 +20,7 @@ public class User {
 	private String username;
 	private String password;
 	private String email;
+	private String real_name;   // 真实姓名，有学术成果的话
+	private String company;     // 所属单位(研究机构或者学校)
+	private String resume;      //
 }
